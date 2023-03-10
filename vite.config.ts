70b271a4b9445b6cb8611384a3b193a9b87bd662
phidnhh <phidnhh@gmail.com>
2023-03-10 20:22:53 +0700

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths'
+import path from 'path'
+
+// https://vitejs.dev/config https://vitest.dev/config
+export default defineConfig({
+  plugins: [react(), tsconfigPaths()],
+  server: {
+    port: 3004
+  },
+  css: {
+    devSourcemap: true
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  }
+})
